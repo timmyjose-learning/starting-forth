e7eac902621f835743835f1099c4fcbeb9544252
Timmy Jose <zoltan.jose@gmail.com>
2026-01-06 15:41:29 +0530

    chapter 1

---
+++ ex11.fs
@@ -0,0 +1,7 @@
+( Chapter 1 exercises )
+
+: gift   ( --  )   ." Bookends" ;
+: giver   ( -- )   ." Stephanie" ;
+: spaces   ( n --  )   0 do space loop ;
+: margin   ( --  )   5 spaces ;
+: thanks   ( --  )   cr ." Dear " giver ." , " cr margin ." Thanks for the " gift ." . " cr  ;
---
+++ ex12.fs
@@ -0,0 +1,3 @@
+( Chapter 1, exercise 2 )
+
+: ten.less   ( n -- n )   -10 + ;
