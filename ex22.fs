f9a2fb022ad3f7bac248fcefd3a47b421fc0cf1d
Timmy Jose <zoltan.jose@gmail.com>
2026-01-07 14:34:11 +0530

    chapter 2: ex22 (updated), /mod and mod

--- ex22.fs
+++ ex22.fs
@@ -17,3 +17,6 @@
 \ a (2a + 3) -> a a 2 * 3 + *
 
 : 2b5  ( a a -- result ) 2 * 3 + * ;
+
+\ (a - b) / c -> a b - c /
+\ So, not really possible to have a "point-free" form without stack manipulation operators
---
+++ quarters.fs
@@ -0,0 +1,4 @@
+\ /mod   ( u1 u2 -- u-rem u-quot )
+\ mod   ( u1 u2 -- u-rem )
+
+: quarters   ( u --  ) cr  4 /mod . ." ones and " . ." quarters" cr ;
