b9c6b4ea94c41e40bf62b5d4e7b6ed74b68c5d79
Timmy Jose <zoltan.jose@gmail.com>
2026-01-07 14:25:20 +0530

    chapter 2: ex22

---
+++ ex22.fs
@@ -0,0 +1,19 @@
+\ ab + c -> c a b * +
+
+: 2b1  ( c b a -- result )   * + ;
+
+\ (a - 4b) / 6 + c -> c a b 4 * - 6 / +
+
+: 2b2  ( c a b -- result  )   4 * - 6 / + ;
+
+\ a / 8b -> a b 8 * / 
+
+: 2b3   ( a b -- result ) 8 * / ;
+
+\ 0.5ab / 100 -> a b * 200 / 
+
+: 2b4   ( a b -- result ) * 200 / ;
+
+\ a (2a + 3) -> a a 2 * 3 + *
+
+: 2b5  ( a a -- result ) 2 * 3 + * ;
